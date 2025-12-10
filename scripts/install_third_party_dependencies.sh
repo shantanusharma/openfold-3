38ee8e3b5fec060ea7546e85d830fe51108adc41
Jan Domanski <jandom@gmail.com>
2025-12-10 09:56:54 +0100

    more comments

--- scripts/install_third_party_dependencies.sh
+++ scripts/install_third_party_dependencies.sh
@@ -5,6 +5,7 @@
 # -o pipefail: Exit if any command in a pipeline fails (not just the last one)
 set -euo pipefail
 
+# These are necessary for subsequent (runtime) compilation of Deepspeed
 echo "Download CUTLASS, required for Deepspeed Evoformer attention kernel"
 git clone https://github.com/NVIDIA/cutlass --branch v3.6.0 --depth 1
 conda env config vars set CUTLASS_PATH=$PWD/cutlass
@@ -12,6 +13,7 @@ conda env config vars set CUTLASS_PATH=$PWD/cutlass
 # This setting is used to fix a worker assignment issue during data loading
 conda env config vars set KMP_AFFINITY=none
 
+# These will only be available outside of this script if it's sourced in the current shell
 export LIBRARY_PATH=$CONDA_PREFIX/lib:$LIBRARY_PATH
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 export PATH=$PATH:/sbin  # TODO: Check if this is necessary, or is NERSC-specific
